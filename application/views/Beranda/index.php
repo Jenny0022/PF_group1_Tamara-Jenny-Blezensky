b149fe1aaec60a57306b0e24d43e8d311dddf064
jennyy0022 <tamarajennyblezensky@gmail.com>
2024-09-19 00:37:28 +0700

    first commit

---
+++ application/views/Beranda/index.php
@@ -0,0 +1,103 @@
+<!-- header -->
+
+<div class="container">
+
+<div id="carouselExampleCaptions" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="application\assets\images\img 1.jpeg" class="d-block w-100" alt="..."style="width: 550px; height: 600px; object-fit: cover;">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>NEXT</h5>
+        <p>"Libraries are the treasure chests of civilization, filled with the power of ideas and dreams."</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="application\assets\images\img 2.jpeg" class="d-block w-100" alt="..."style="width: 550px; height: 600px; object-fit: cover;">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>LIBRARY</h5>
+        <p>"Books give a soul to the universe, wings to the mind, flight to the imagination, and life to everything."</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="application\assets\images\img 3.jpeg" class="d-block w-100" alt="..."style="width: 550px; height: 600px; object-fit: cover;">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>ROOM</h5>
+        <p>"The library is the temple of learning, and learning has liberated more people than all the wars in history."</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</div>
+<!-- end header -->
+
+<!-- about -->
+<section class="about" id="about">
+<div class="container">
+  <div class="row mt-5">
+    <div class="col-md-12 text center">
+      <div class="text-center">
+        <h2>ABOUT</h2>
+      <hr>
+    </div>
+  </div>
+    <div class="row justify-content-center">
+  <div class="col-md-5">
+    <p>A library is a place where books, digital resources, and other materials are stored and made available for people to read, research, or study. It promotes learning, knowledge-sharing, and community engagement. In addition to books, modern libraries offer access to computers, online databases, workshops, and community events, fostering lifelong learning. Libraries often serve as quiet spaces for study and collaboration, making them essential community hubs.</p>
+  </div>
+  <div class="col-md-5">
+    <p>Books offer a unique perspective on life by expanding knowledge, fostering empathy, and sparking imagination. They allow readers to explore different cultures, time periods, and experiences, offering new insights and broadening worldviews. Through books, life's complexities become more understandable and enriching.</p>
+  </div>
+</div>
+</div>
+</section>
+<!-- end about -->
+
+<!-- contact -->
+<section class="contact" id="contact">
+  <div class="container">
+    <div class="row mt-5">
+      <div class="col-md text-center">
+          <h2>JOIN US!</h2>
+          <hr>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-md-5">  
+      <form>
+  <div class="form-group">
+    <label for="Name">Name</label>
+    <input type="text" class="form-control" id="Name" placeholder="Insert name">
+  </div>
+  <div class="form-group">
+    <label for="Name">Email</label>
+    <input type="text" class="form-control" id="Email" placeholder="Insert email">
+  </div>
+  <div class="form-group">
+    <label for="Name">Address</label>
+    <input type="text" class="form-control" id="Address" placeholder="Insert address">
+  </div>
+  <div class="form-group mb-3">
+    <label for="Name">Message</label>
+    <textarea type="textarea" class="form-control" id="Message" placeholder="Insert message"></textarea>
+  </div>
+    <button type="submit" class="btn btn-primary">Send</button>
+</form>
+    </div>
+  </div>
+  </div>
+  </div>
+</section>
+<!-- end contact -->
